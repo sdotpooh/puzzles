2b766fe1f2a0f704b194f7070035adb1818d7047
sdotpooh <vinassr@gmail.com>
2014-03-02 01:01:55 -0500

    Updated jugglefest.py

--- jugglefest.py
+++ jugglefest.py
@@ -19,6 +19,7 @@ class Circuits:
         self.preferenceList = []
     def openings(self):
         return self.positions-len(self.partners)
+        
     def removePartner(self, pos):
         #Remove specific partner and dot value
         self.partners.pop(pos)
@@ -37,56 +38,7 @@ class Jugglers:
 def dot(a, b, c, d, e, f):
     return a*d+b*e+c*f
 
-def main():
-    start = time.time()
-    circuits = []
-    jugglers = []
-    filename = 'jugglefest.txt'
-    pListLen = 0
-    # Read text file and add data
-    for line in fileinput.input([filename]): 
-        if line.strip():
-            if line.split()[0] == 'C':
-                circuits.append(Circuits(line.split()[1], 
-                                         int(line.split()[2].strip('H:')), 
-                                         int(line.split()[3].strip('E:')), 
-                                         int(line.split()[4].strip('P:'))))
-            else:
-                jh = int(line.split()[2].strip('H:'))
-                je = int(line.split()[3].strip('E:'))
-                jp = int(line.split()[4].strip('P:'))
-                pList = list(line.split()[5].split(','))
-                if pListLen == 0:
-                    pListLen = len(pList) # Move out of loop for better speed
-                jugglers.append(
-                    Jugglers(line.split()[1], jh, je, jp, pList,
-                             [dot(jh, je, jp, 
-                                  circuits[int(pList[x].strip('C'))].h, 
-                                  circuits[int(pList[x].strip('C'))].e, 
-                                  circuits[int(pList[x].strip('C'))].p) 
-                              for x in xrange(pListLen)]))           
-    fileinput.close()
-
-    # Build a list of tuples of Sorted jugglers 
-    jSize = len(jugglers)
-    cSize = len(circuits)
-    availablePositions = jSize/cSize
-    for i in xrange(cSize):
-        # Assign each circuit its dot sorted preferenceList
-        # as a List of Tuples; highest ranking at the end
-        circuits[i].preferenceList = (
-            sorted(zip([dot(circuits[i].h,
-                            circuits[i].e,
-                            circuits[i].p,
-                            jugglers[j].h, 
-                            jugglers[j].e, 
-                            jugglers[j].p) 
-                         for j in xrange(jSize)], 
-                        [jugglers[k].name for k in xrange(jSize)])))
-
-        # Assign each circuit with a quantity of openings available (J/C)
-        circuits[i].positions = availablePositions
-
+def galeShapleyAssignmentMatching(cSize, jSize, circuits, jugglers):
     # Gale Shapley - Matching Algorithm
     totalOpenPositions = jSize
     i = -1
@@ -137,9 +89,56 @@ def main():
                         c.pDots.append(d)
                         cIndex.partners.remove(j)
                         jIndex.partner = c.name
-                   
-    # Format output text file
+
+    return circuits, jugglers
+
+def readInputTextFile(fileName, circuits, jugglers, pListLen):
+    for line in fileinput.input([fileName]): 
+        if line.strip():
+            if line.split()[0] == 'C':
+                circuits.append(Circuits(line.split()[1], 
+                                         int(line.split()[2].strip('H:')), 
+                                         int(line.split()[3].strip('E:')), 
+                                         int(line.split()[4].strip('P:'))))
+            else:
+                jh = int(line.split()[2].strip('H:'))
+                je = int(line.split()[3].strip('E:'))
+                jp = int(line.split()[4].strip('P:'))
+                pList = list(line.split()[5].split(','))
+                if pListLen == 0:
+                    pListLen = len(pList) # Move out of loop for better speed
+                jugglers.append(
+                    Jugglers(line.split()[1], jh, je, jp, pList,
+                             [dot(jh, je, jp, 
+                                  circuits[int(pList[x].strip('C'))].h, 
+                                  circuits[int(pList[x].strip('C'))].e, 
+                                  circuits[int(pList[x].strip('C'))].p) 
+                              for x in xrange(pListLen)]))           
+    fileinput.close()
+    return circuits, jugglers, pListLen
+
+def sortCircuitsPreferences(cSize, jSize, circuits, jugglers):
+    availablePositions = jSize/cSize
+    for i in xrange(cSize):
+        # Assign each circuit its dot sorted preferenceList
+        # as a List of Tuples; highest ranking at the end
+        circuits[i].preferenceList = (
+            sorted(zip([dot(circuits[i].h,
+                            circuits[i].e,
+                            circuits[i].p,
+                            jugglers[j].h, 
+                            jugglers[j].e, 
+                            jugglers[j].p) 
+                         for j in xrange(jSize)], 
+                        [jugglers[k].name for k in xrange(jSize)])))
+ 
+        # Assign each circuit with a quantity of openings available (J/C)
+        circuits[i].positions = availablePositions
+    return circuits, jugglers
+
+def outputDataToTextFile(cSize, jSize, pListLen, circuits, jugglers):
     f = open("output.txt", "w")
+    availablePositions = jSize/cSize
     for xx in xrange(cSize):
         f.write(circuits[xx].name)
         for x in xrange(availablePositions):
@@ -156,17 +155,60 @@ def main():
         f.write(' ')
         f.write('\n')
     f.close()
+    return
+
+def whoToEmailAtYodle(cSize, jSize, circuitNumber, circuits):
+    sol = 0
+    cOut = circuitNumber
+    for x in xrange(jSize/cSize):
+        sol += int(circuits[cOut].partners[x].strip('J'))
+    print sol,'@yodle.com'
+    return
+
+def main():
+    start = time.time()
+    circuits = []
+    jugglers = []
+    fileName = 'jugglefest.txt'
+    pListLen = 0
+
+    # Read text file and extract data
+    (circuits, jugglers, pListLen) = readInputTextFile(fileName, 
+                                                       circuits, 
+                                                       jugglers, 
+                                                       pListLen)
+ 
+    jSize = len(jugglers)
+    cSize = len(circuits)
+
+    # Sort each circuits jugglers by calculating 
+    # each dot product, then sorting
+    (circuits, jugglers) = sortCircuitsPreferences(cSize, 
+                                                   jSize, 
+                                                   circuits, 
+                                                   jugglers)
+    # Gale Shapley Algorithm
+    # Matches jugglers to circuits based on prefernces
+    # and dot product of skills
+    (circuits, jugglers) = galeShapleyAssignmentMatching(cSize, 
+                                                         jSize, 
+                                                         circuits, 
+                                                         jugglers)
+     
+    # Format output text file to current directory
+    # output file = output.txt
+    outputDataToTextFile(cSize, jSize, pListLen, circuits, jugglers)
     
     # Email to: 
-    sol = 0
-    cOut = 1970
-    for x in xrange(availablePositions):
-        sol += int(circuits[cOut].partners[x].strip('J'))
-    print circuits[cOut].name, sol
-    print sol,'@yodle.com'
+    # Function sums the jugglers id's to create the who to email
+    circuitNumber = 1970
+    whoToEmailAtYodle(cSize, jSize, circuitNumber, circuits)
+    
+    # General time analysis
     stop = time.time()
     print stop - start
 
 if __name__ == '__main__':
     main()
+
 
